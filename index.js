20730d2a6b46522d22f3f540a7ac3201ad94d779
harsha19git <96284430+harsha19git@users.noreply.github.com>
2022-02-04 22:24:42 +0530

    added meditation pages and few other changes as well

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let fog_front = document.getElementById('fog_front');
 let fog_front1 = document.getElementById('fog_front1');
-let text = document.getElementById('text');
+let heading = document.getElementById('heading');
+let day = document.getElementById('day');
 
 
 window.addEventListener('scroll' , function(){
 let value = window.scrollY;
 
-fog_front.style.left = value * 0.35 + 'px';
-fog_front1.style.left = value * 1.25 + 'px'; 
-text.style.marginRight = value * 2 + 'px';
+fog_front.style.left = value * 1.35 + 'px';
+fog_front1.style.left = value * 0.25 + 'px'; 
+heading.style.marginRight = value * 2 + 'px';
+day.style.left = value * 0.5 + 'px';
 });
